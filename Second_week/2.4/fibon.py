b93c39c72ab2d4cbd9b37f49ace56504a5311316
ibrikin <ibrikin@gmail.com>
2014-07-15 23:24:55 +0400

    Add task 2.4

---
+++ Second_week/2.4/fibon.py
@@ -0,0 +1,15 @@
+def fibn(n):
+    if n == 0: return n
+    if n == 1: return n
+    numFirst = 0
+    numSecond = 1
+    result = 1
+    count = 1
+    while count < n:
+        result = numFirst + numSecond
+        numFirst = numSecond 
+        numSecond = result 
+        count += 1
+    return result
+x = fibn(10)
+print(x)
