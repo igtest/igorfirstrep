5a31a338f50cc406a255a61e2da9810241551a72
ibrikin <ibrikin@gmail.com>
2014-07-15 13:43:39 +0400

    First commit.Add task first week

---
+++ First_week/1.1/GuessNumber.py
@@ -0,0 +1,26 @@
+import random
+randomInt = random.randint(0,100)
+
+print("I think of a number...Guess!")
+
+flag = 0
+count = 0
+while flag == 0:
+	print("Input your value: ")
+	count += 1
+	value = input()
+	s = int(value)
+	#print(s)
+	if  s > randomInt:
+		print("Your value is bigger")
+		continue
+	if s < randomInt:
+		print("Your value is less")
+		continue
+	if s == randomInt:
+		print("You are guess!")
+		flag = 1
+
+print("I think of a number " + repr(randomInt))
+print("You made " + repr(count) + " attempts")
+
