294bf17e0883b79dfc9cf5f218253c373c5b7f53
ibrikin <ibrikin@gmail.com>
2014-07-16 10:11:41 +0400

    Change rep

--- Second_week/2.2/fibo.py
+++
@@ -1,8 +0,0 @@
-def fibo(n):
-        print("Начинаем вычислять число Фибоначи "+str(n))
-        if n == 0: result = 0
-        elif n == 1: result = 1
-        else: result = fibo(n - 1) + fibo(n-2)
-        return result
-x = fibo(10)
-print(x)
