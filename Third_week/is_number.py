15deafe00ccb4a112ef68864e7f3e47111044a71
ibrikin <ibrikin@gmail.com>
2014-07-20 14:08:38 +0400

    Add task 3.1

---
+++ Third_week/is_number.py
@@ -0,0 +1,19 @@
+def is_number(string):
+    index = 0
+    subString = ""
+    if string.isdigit():
+        return True
+    if string[0] == '-':
+        subString = string[1:]
+    else:
+        subString = string
+    if '.' in subString:
+        index = subString.index('.')
+        subString = subString[:index] + subString[index+1:]
+    elif ',' in sunString:
+        index = subString.index(',')
+        subString = subString[:index] + subString[index+1:]
+    if subString.isdigit():
+        return True
+    else:
+        return False
