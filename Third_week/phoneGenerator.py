6d40a0ed61bc57390d2df3385fc358908635331a
ibrikin <ibrikin@gmail.com>
2014-07-22 15:25:12 +0400

    Add some change in phoneGenerate

--- Third_week/phoneGenerator.py
+++ Third_week/phoneGenerator.py
@@ -9,7 +9,7 @@ def return_name():
     name = ''.join(random.choice(chars) for x in range(size))
     return name
 
-def gen_phoneBook()
+def gen_phoneBook():
     phone_book = []
     index = 0
     while index < 100000:
@@ -26,16 +26,13 @@ def gen_phoneBook()
     f.close()
 
 def gen_phoneBookFaster():
-    numbers = set()
-    while len(numbers) < 100000:
+    phone_book = set()
+    while len(phone_book) < 100000:
         phone = return_number()
         name = return_name()
-        if phone in numbers or name in numbers:
-            continue
-        else:
-            numbers.add((phone, name))
+        phone_book.add((phone, name))
     f = open('phone_new.txt', 'w')
-    for index in numbers:
+    for index in phone_book:
         f.write(str(index) + '\n')
     f.close()
 
