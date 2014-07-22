3513903c75aecc7de2d91f27ac54daa086191dfe
ibrikin <ibrikin@gmail.com>
2014-07-22 13:47:28 +0400

    Add more faster function in phoneGenerator

--- Third_week/phoneGenerator.py
+++ Third_week/phoneGenerator.py
@@ -9,17 +9,35 @@ def return_name():
     name = ''.join(random.choice(chars) for x in range(size))
     return name
 
-phone_book = []
-index = 0
-while index < 100000:
-    phone = return_number()
-    name = return_name()
-    if phone in phone_book or name in phone_book:
-        continue
-    else:
-        phone_book.append([phone,name])
-        index += 1
-f = open('phone.txt', 'w')
-for index in phone_book:
-    f.write(str(index) + '\n')
-f.close()
+def gen_phoneBook()
+    phone_book = []
+    index = 0
+    while index < 100000:
+        phone = return_number()
+        name = return_name()
+        if phone in phone_book or name in phone_book:
+            continue
+        else:
+            phone_book.append([phone,name])
+            index += 1
+    f = open('phone.txt', 'w')
+    for index in phone_book:
+        f.write(str(index) + '\n')
+    f.close()
+
+def gen_phoneBookFaster():
+    numbers = set()
+    while len(numbers) < 100000:
+        phone = return_number()
+        name = return_name()
+        if phone in numbers or name in numbers:
+            continue
+        else:
+            numbers.add((phone, name))
+    f = open('phone_new.txt', 'w')
+    for index in numbers:
+        f.write(str(index) + '\n')
+    f.close()
+
+gen_phoneBookFaster()
+
