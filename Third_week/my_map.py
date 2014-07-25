3672ba174bed1f728e46ff58442225311b2ada3c
ibrikin <ibrikin@gmail.com>
2014-07-25 09:16:12 +0400

    Add my_map function

---
+++ Third_week/my_map.py
@@ -0,0 +1,18 @@
+somelist = [x for x in range(-4, 20)]
+
+
+def my_map(Func, arg):
+    result = []
+    for i in arg:
+        result.append(Func(i))
+    return result
+
+def my_filter(Func, arg):
+    result = []
+    for i in arg:
+        if Func(i) == True:
+            result.append(i)
+    return result
+
+print(my_map(lambda x: x**2, my_filter(lambda x: 2<x<10, somelist)))
+print(my_filter(lambda x: 2<x<10, somelist))
