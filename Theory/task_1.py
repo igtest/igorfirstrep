c6c5c746d607e50ddbeffa8618cf97855fceebf3
ibrikin <ibrikin@gmail.com>
2014-07-28 14:48:42 +0400

    Change task_1

--- Theory/task_1.py
+++ Theory/task_1.py
@@ -1,6 +1,10 @@
-def to_octo(x):
-    res = ""
-    while x:
-        res += str(x%8)
-        x //= 8
-    return res[::-1]
+def octoToDec(octoNum):
+    length = len(octoNum)
+    value = 0
+    for x in octoNum:
+        if int(x) > 7:
+            return -1
+        length -= 1
+        value += int(x) * (8**length)
+    return value
+print(octoToDec('215627'))
